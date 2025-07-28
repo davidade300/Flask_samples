acc4c695cccc1d0f9f7001ac3c65d5c04f5566d3
davidade300 <iaderinho@icloud.com>
2025-07-28 13:33:56 -0300

    dynamic endpoints added

--- flask_study/app.py
+++ flask_study/app.py
@@ -23,7 +23,24 @@ def get_tasks() -> Response:
 def add_task():
     new_task = request.json
     tasks.append(new_task)
-    return jsonify(new_task), 201
+    return (jsonify(new_task), 201)
+
+
+@app.route("/api/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = next((task for task in tasks if task["id"] == task_id), None)
+
+    if task:
+        task.update(request.json)
+        return jsonify(task)
+
+    return (jsonify({"error": "Task not found"}), 404)
+
+
+# captures an integer from the url and passes it to the function
+@app.route("/user/<int:user_id>")
+def user_profile(user_id):
+    return f"User Id: {user_id}"
 
 
 if __name__ == "__main__":
